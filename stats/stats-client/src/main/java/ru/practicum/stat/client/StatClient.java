92dbcab957c3fd19c9da4360c819d1d3db7b72b7
Denis Panasyuk <den.dzim@yandex.ru>
2026-07-19 20:17:07 +0300

    добавлен и настроен DiscoveryServer

--- stats/stats-client/src/main/java/ru/practicum/stat/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stat/client/StatClient.java
@@ -4,11 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -23,15 +28,18 @@ import java.util.List;
 @Component
 public class StatClient {
 
-	private final String serverUrl;
-
+	private final String statsServiceId;
 	private final RestTemplate rest;
+	private final DiscoveryClient discoveryClient;
+	private final RetryTemplate retryTemplate;
 
-	public StatClient(@Value("${stats-server.url:http://localhost:9090}") String serverUrl,
-	                  RestTemplateBuilder builder) {
-		this.serverUrl = serverUrl;
+	public StatClient(DiscoveryClient discoveryClient, RestTemplateBuilder builder,
+					  @Value("${stats-service.id:STATS-SERVER}") String statsServiceId) {
+		this.statsServiceId = statsServiceId;
+		this.discoveryClient = discoveryClient;
 		this.rest = builder.build();
-	}
+		this.retryTemplate = createRetryTemplate();
+    }
 
 	public void hit(EndpointHitDto endpointHitDto) {
 		try {
@@ -41,7 +49,7 @@ public class StatClient {
 			HttpEntity<EndpointHitDto> requestEntity = new HttpEntity<>(endpointHitDto, headers);
 
 			rest.exchange(
-					serverUrl + "/hit",
+					makeUriString("/hit"),
 					HttpMethod.POST,
 					requestEntity,
 					Void.class
@@ -55,7 +63,7 @@ public class StatClient {
 	public List<ViewStatsDto> getStat(StatsRequest statsRequest) {
 		try {
 			UriComponentsBuilder builder = UriComponentsBuilder
-					.fromHttpUrl(serverUrl + "/stats")
+					.fromHttpUrl(makeUriString("/stats"))
 					.queryParam("start", statsRequest.getStart())
 					.queryParam("end", statsRequest.getEnd())
 					.queryParam("unique", statsRequest.getUnique());
@@ -80,5 +88,33 @@ public class StatClient {
 			log.error("Ошибка записи: {}", statsRequest, e);
 			return null;
 		}
+	}
+
+	private ServiceInstance getInstance() {
+		try {
+			return discoveryClient
+					.getInstances(statsServiceId)
+					.getFirst();
+		} catch (Exception exception) {
+			throw new RuntimeException("Ошибка обнаружения адреса сервиса статистики с id: " + statsServiceId, exception);
+		}
+	}
+
+	private String makeUriString(String path) {
+		ServiceInstance instance = retryTemplate.execute(context -> getInstance());
+		return "http://" + instance.getHost() + ":" + instance.getPort() + path;
+	}
+
+	private RetryTemplate createRetryTemplate() {
+		RetryTemplate template = new RetryTemplate();
+
+		FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+		fixedBackOffPolicy.setBackOffPeriod(3000L);
+		template.setBackOffPolicy(fixedBackOffPolicy);
+
+		MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+		retryPolicy.setMaxAttempts(3);
+		template.setRetryPolicy(retryPolicy);
+		return template;
 	}
 }
