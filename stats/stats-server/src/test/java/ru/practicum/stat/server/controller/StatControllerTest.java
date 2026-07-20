a0d6cecd37d116d7a434527f9cd36fa386730ed5
Denis Panasyuk <den.dzim@yandex.ru>
2026-07-20 17:22:47 +0300

    правки

--- stats/stats-server/src/test/java/ru/practicum/stat/server/controller/StatControllerTest.java
+++ stats/stats-server/src/test/java/ru/practicum/stat/server/controller/StatControllerTest.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.stat.dto.EndpointHitDto;
 import ru.practicum.stat.server.service.StatService;
@@ -27,7 +27,7 @@ class StatControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@MockitoBean
+	@MockBean
 	private StatService statsService;
 
 	@Test
