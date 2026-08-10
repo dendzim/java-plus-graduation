7713a84794083e119201857d328a609ed0521629
Denis Panasyuk <den.dzim@yandex.ru>
2026-08-10 17:53:32 +0300

    Исправлен запуск Collector

--- stats/collector/src/main/java/ru/practicum/ewm/stats/collector/CollectorApplication.java
+++ stats/collector/src/main/java/ru/practicum/ewm/stats/collector/CollectorApplication.java
@@ -1,7 +1,11 @@
 package ru.practicum.ewm.stats.collector;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@SpringBootApplication
 public class CollectorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
